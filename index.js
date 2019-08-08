202f12f1d91f1b7358b320689a0e6b258b5b230e
Jo Knightstep <jocoleknight@gmail.com>
2019-08-07 20:02:42 -0500

    js

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-const { watchViewport } = tornis;
-$text2 = ('.panel2 h1');
-$text1 = ('.panel1 h1');
-$inner1 = ('.panel1 .inner');
-$inner2 = ('.panel2 .inner');
-$filter1 = $("#wave1 #displacement");
-$filter2 = $("#wave2 #displacement");
-$distance = $('.panel2 h1').offset().top,
-$window = $(window);
-$elHeight = ($('.panel2 h1').height() * 2);
+
+// Parallax
+if('ontouchstart' in window == false){
+  $('body').mousemove(function(e){
+    var moveX = (e.pageX * -1 / 30);
+    var moveY = (e.pageY * -1 / 30);
+    $('.intro').css('background-position', moveX + 'px ' + moveY + 'px');
+  });
+}
 
 
+$(window).scroll(function(){    
+  var scrollTop = $(window).scrollTop();
+  
+  $('#button').css({'margin-top' : scrollTop / 1.25 + 200 + 'px'});
+  
+  $('.welcome').css({'margin-top' : scrollTop / 1.1 + 0 + 'px'});
+  
+  $('.about').css({'margin-top' : scrollTop / 1.2 + 125 + 'px'});
+  
+  $('.one').addClass('on'); 
+  $('.two').addClass('on'); 
+  $('.three').addClass('on'); 
+  
+  setTimeout(function() {
+    $('.one').addClass('on'); 
+    setTimeout(function() {
+      $('.two').addClass('on'); 
+      setTimeout(function() {
+        $('.three').addClass('on'); 
+      }, 300)
+    }, 300)
+  }, 300)
+});
 
-const updateValues = ({ scroll }) => {
 
-  if (scroll.changed) {
-    $scaleOffset = (scroll.top / 3);
-    $scaleOffset2 = (($scaleOffset/100) + 1);
-    $scaleOffset3 = (((window.innerHeight + 0) - scroll.top) / 3);
-    $scaleOffset4 = (($scaleOffset3/100) + 1);
-    TweenMax.to($filter1, 0, { ease: Expo.easeOut, attr: { scale: $scaleOffset } });
-    TweenMax.to($inner1, 0, { ease: Elastic.easeOut.config(1, 0.3), css: { transform: "scale("+$scaleOffset2+") translateY(" + ($scaleOffset * -0.75) + "px)" } })
-    TweenMax.to($text1, 0, { ease: Elastic.easeOut.config(1, 0.3), css: { transform: "translateX(" + ($scaleOffset / -2) + "px) translateY(" + ($scaleOffset * 0.75) + "px) scaleY("+$scaleOffset2+")" } });
-    if ( $window.scrollTop() < ($distance - ((window.innerHeight/2) - $elHeight)) ) {
-      TweenMax.to($filter2, 0, { ease: Expo.easeOut, attr: { scale: $scaleOffset3 } });
-      TweenMax.to($inner2, 0, { ease: Elastic.easeOut.config(1, 0.3), css: { transform: "scale("+$scaleOffset4+") translateY(" + ($scaleOffset3 * 0.75) + "px)" } })
-      TweenMax.to($text2, 0, { ease: Elastic.easeOut.config(1, 0.3), css: { transform: "translateX(" + ($scaleOffset3 / -2) + "px) translateY(" + ($scaleOffset3 * 0.75) + "px) scaleY("+$scaleOffset4+")" } });
-    }else{
-      TweenMax.to($filter2, 2, { ease: Elastic.easeOut.config(1, 0.3), attr: { scale: 0 } });
-      TweenMax.to($text2, 2, { ease: Elastic.easeOut.config(1, 0.3), css: { transform: "translateX(0) translateY(0) scaleY(1)" } });
-    }
-  }
-};
+$(window).scroll(function(){    
+  var scrollTop = $(window).scrollTop();
 
-watchViewport(updateValues);
+  
+  
+});
